2eeecf99a702553651507d89bedd8c7f847a4300
Theu <info@matteocontini.com>
2017-04-17 11:03:23 +0200

    REA-01 a lot of stuff

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -6,6 +6,8 @@ import Inventory from './Inventory';
 class App extends React.Component {
     constructor() {
         super();
+
+        this.addFish = this.addFish.bind(this);
         // getinitialstate
         this.state = {
             fishes: {},
@@ -14,8 +16,14 @@ class App extends React.Component {
     }
     addFish(fish) {
         //update our state
-        
+        const fishes = {...this.state.fishes}  // the spread operator thake a copy of all elemnt and insert in the object
+        // ad in our new fish
+        const timestamp = Date.now();
+        fishes[`fish-${timestamp}`] = fish;
+        // this.state.fishes.fish1 = fish --> is possible but unsafe. better to store a copy of the state before to update it as done above
         // set state
+        // this.setState({ fishes: fishes }) same of
+        this.setState({ fishes })
     }
     render() {
         return (
@@ -24,7 +32,7 @@ class App extends React.Component {
                     <Header age="5000" cool={true} tagline="Fresh Seafood market" />
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish} />
             </div>
         )
     }
