5743ac758b0668a482c606966ea6456088adb5ba
Matteo Contini <matteo.contini@zalando-lounge.de>
2017-04-19 18:41:52 +0200

    REA-20 canged inventory and made prod and order able to read the change

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -12,8 +12,10 @@ class App extends React.Component {
         super();
 
         this.addFish = this.addFish.bind(this);
+        this.updateFish = this.updateFish.bind(this);
         this.loadSamples = this.loadSamples.bind(this);
         this.addToOrder = this.addToOrder.bind(this);
+        
         
         // getinItialState
         this.state = {
@@ -59,6 +61,13 @@ class App extends React.Component {
         // this.setState({ fishes: fishes }) same of
         this.setState({ fishes })
     }
+
+    updateFish(key, updatedFish) {
+        const fishes = {...this.state.fishes};
+        fishes[key] = updatedFish;
+        this.setState({ fishes })
+    }
+
     loadSamples() {
         this.setState({
             fishes: sampleFishes
@@ -94,6 +103,7 @@ class App extends React.Component {
                     addFish={this.addFish} 
                     loadSamples={this.loadSamples} 
                     fishes={this.state.fishes}
+                    updateFish={this.updateFish}
                 />
             </div>
         )
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -4,22 +4,32 @@ import AddFishForm from './AddFishForm';
 class Inventory extends React.Component {
     constructor() {
         super();
-        this.renderInventory = this.renderInventory.bind(this)
+        this.renderInventory = this.renderInventory.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e, key) {
+        const fish = this.props.fishes[key];
+        // take a copy of that fish and update it with new data
+        const updatedFish = {
+            ...fish,
+            [e.target.name]: e.target.value // this is a computed property. prende il valore che é stato cambviato e ne stor il cambiamento
+        }
+        this.props.updateFish(key, updatedFish)
     }
     renderInventory(key) {
         const fish = this.props.fishes[key];
         return (
             <div className="fish-edit" key={key}>
-                <input type="text" name="name" defaultValue={fish.name} placeholder="Fish name" />
-                <input type="text" name="price" defaultValue={fish.price} placeholder="Fish price" />
-                <select type="text" name="status" defaultValue={fish.status} placeholder="Fish status">
+                <input type="text" name="name" defaultValue={fish.name} placeholder="Fish name" onChange={(e) => this.handleChange(e, key)} />
+                <input type="text" name="price" defaultValue={fish.price} placeholder="Fish price" onChange={(e) => this.handleChange(e, key)} />
+                <select type="text" name="status" defaultValue={fish.status} placeholder="Fish status" onChange={(e) => this.handleChange(e, key)}>
                     <option value="available">Fresh!</option>
                     <option value="unavailable">Sold Out!</option>
                 </select>
-                <textarea type="text" name="desc" defaultValue={fish.desc} placeholder="Fish desc">
+                <textarea type="text" name="desc" defaultValue={fish.desc} placeholder="Fish desc" onChange={(e) => this.handleChange(e, key)}>
 
                 </textarea>
-                <input type="text" name="image" defaultValue={fish.image} placeholder="Fish image" />
+                <input type="text" name="image" defaultValue={fish.image} placeholder="Fish image" onChange={(e) => this.handleChange(e, key)} />
             </div>
         )
     } 
